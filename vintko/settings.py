30cb1be4120ae8964de54575d891624eb1ed7dc7
skrolz <>
2022-09-04 20:09:26 -0400

    more testing

--- vintko/settings.py
+++ vintko/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'resource',
-    'watch_list',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resource',
+    'watch_list',
 ]
 
 MIDDLEWARE = [
