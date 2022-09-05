9f90385ac37aebd047b06b243963ac96b2219a8c
skrolz <>
2022-09-04 23:57:04 -0400

    misc

--- vintko/settings/base.py
+++ vintko/settings/base.py
@@ -20,10 +20,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- vintko/settings/prod.py
+++ vintko/settings/prod.py
@@ -1,6 +1,5 @@
 import django_on_heroku
 from decouple import config
-
 from .base import *
 
 SECRET_KEY = config('SECRET_KEY')
