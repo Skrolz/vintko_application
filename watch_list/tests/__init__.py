930639d6bab52f434ba093b73ee901032ae490aa
skrolz <>
2022-09-04 22:48:02 -0400

    cleanup

--- watch_list/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from .brand_tests import BrandTests
-from .watch_tests import WatchTests
-from .image_tests import ImageTests
-from .value_type_tests import ValueTypeTests
-from .material_type_tests import MaterialTypeTests
-from .movement_type_tests import MovementTypeTests
-from .value_tests import ValueTests
--- watch_list/tests/value_tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-
-class ValueTests(TestCase):
-    pass
