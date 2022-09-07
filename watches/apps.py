bdc14bae8c26df67d10003af500f9e645449d69e
skrolz <>
2022-09-06 20:46:34 -0400

    renamed config class

--- watches/apps.py
+++ watches/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class WatchListConfig(AppConfig):
+class WatchesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'watches'
-    verbose_name = 'Watch List'
