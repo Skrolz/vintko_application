9f64e6399b5bd785330f78d1e089a45386f24bf4
skrolz <>
2022-09-07 21:36:38 -0400

    fixed readonly fields bug

--- watches/admin/value_type_admin.py
+++ watches/admin/value_type_admin.py
@@ -9,10 +9,12 @@ class ValueTypeAdmin(admin.ModelAdmin):
     inlines = [ValueInline,]
     list_display  = ('name',)
     ordering = ['name',]
+    readonly_fields = []
 
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
             return [f.name for f in self.model._meta.fields]
+        return self.readonly_fields
 
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
